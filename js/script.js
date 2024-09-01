66b06b28fe9ec22354edea208a57ba44eaed12de
Mirko Rinke <mirkorinke@htp.com>
2024-09-01 18:55:22 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//! Mobile Menu //
+
+function toggleMenu() {
+  document.getElementById("side_menu").classList.toggle("nav_container--nav--menu_mobile--toggle");
+  document.getElementById("overlay").classList.toggle("toggle");
+}
+
+//! Contact Send Mail //
+
+function sendMail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  // https://formspree.io/f/mqazbyjw
+  fetch("https://formspree.io/f/mqazbyjw", {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(() => {
+      window.location.href = "./send_mail.html";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+//! Nav und Footer fetch )
+
+// Fetch für die Navigation  <div id="nav"></div>
+fetch("./components/nav.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("nav").innerHTML = data));
+
+// Fetch für den Footer  <div id="footer"></div>
+fetch("./components/footer.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("footer").innerHTML = data));
+
+//! Berechung Rezept //
+
+const ingredientsTable = document.getElementById("ingredientsTable");
+const original_list = ingredientsTable.innerHTML;
+let = errorMessage = "Bitte geben Sie eine gültige Portionenzahl ein (zwischen 1 und 10).";
+
+if (ingredientsTable) {
+  function CalculatePortions(event) {
+    event.preventDefault();
+    let value = document.getElementById("portions").value;
+    document.getElementById("error-message").innerHTML = "";
+    if (value < 1 || value > 10) {
+      value = 1;
+      document.getElementById("portions").value = 1;
+      document.getElementById("error-message").innerHTML = errorMessage;
+    }
+    let new_list = original_list.replaceAll(/\d+/g, (match) => {
+      return parseInt(match) * value;
+    });
+    document.getElementById("ingredientsTable").innerHTML = new_list;
+  }
+}
